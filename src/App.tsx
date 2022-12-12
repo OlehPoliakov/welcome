5449067cc1bfec194c4ac32e950d4b2c24e98005
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2022-12-12 15:41:43 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import BoardComponent from './components/BoardComponent';
-import LostFiguresComponent from './components/LostFiguresComponent';
-import TimerComponent from './components/TimerComponent';
-import { Board } from './models/Board';
-import { Colors } from './models/Colors';
-import { Player } from './models/Player';
+import React from 'react';
 
-const App = () => {
-  const [board, setBoard] = useState(new Board());
-  const [whitePlayer] = useState(new Player(Colors.WHITE));
-  const [blackPlayer] = useState(new Player(Colors.BLACK));
-  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
-
-  useEffect(() => {
-    restart();
-    setCurrentPlayer(whitePlayer);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  function restart() {
-    const newBoard = new Board();
-    newBoard.initCells();
-    newBoard.addFigures();
-    setBoard(newBoard);
-  }
-
-  function swapPlayer() {
-    setCurrentPlayer(
-      currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer
-    );
-  }
-
-  return (
-    <div className="App">
-      <TimerComponent restart={restart} currentPlayer={currentPlayer} />
-      <BoardComponent
-        board={board}
-        setBoard={setBoard}
-        currentPlayer={currentPlayer}
-        swapPlayer={swapPlayer}
-      />
-      <div>
-        <LostFiguresComponent
-          title={'Black Figures'}
-          figures={board.lostBlackFigures}
-        />
-        <LostFiguresComponent
-          title={'White Figures'}
-          figures={board.lostWhiteFigures}
-        />
-      </div>
-    </div>
-  );
+const App: React.FC = () => {
+  return <div className="App">Welcome</div>;
 };
 
 export default App;
